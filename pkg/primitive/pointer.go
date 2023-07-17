8089ae5fcae5ff5f111e8f242b120faa8bba3ec9
Tom <tartale.work@gmail.com>
2023-07-17 12:25:56 -0600

    Primitive ref support for bool

--- pkg/primitive/pointer.go
+++ pkg/primitive/pointer.go
@@ -6,6 +6,6 @@ package primitive
 //	var foo *int
 //	foo = &1               // illegal
 //	foo = primitive.Ref(1) // legal
-func Ref[T int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64 | string](val T) *T {
+func Ref[T bool | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64 | string](val T) *T {
 	return &val
 }
